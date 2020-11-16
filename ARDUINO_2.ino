0d71f454c3e117effee28a51ef6e27e201fec527
Anderson <anderson97reis@gmail.com>
2020-11-15 22:14:23 -0300

    alteração mediaRound para int e alturaTotal para int

--- ARDUINO_2.ino
+++ ARDUINO_2.ino
@@ -8,13 +8,13 @@ const int echoPin = 10;
 long duration;
 float distanceCm;
 float volumeCm, volumeOcupado;
-const float alturaTotal = 12.10;
+const float alturaTotal = 12;
 const float raioCirc = 4;
 const float areaCirc = 3.14 * (raioCirc * raioCirc);
 const float volumeTotal = alturaTotal * areaCirc;
 const float densidade = 1.99;
 float massaG;
-float mediaRound = 0; 
+int mediaRound = 0; 
 //primeira media das leituras
 const int bufferSize1 = 16;
 float ultraBuffer1[bufferSize1];
@@ -28,18 +28,6 @@ float ultraBuffer2[bufferSize2];
 int bufferIndex2 = 0;
 int bufferLength2 = 0; 
 float mediaUltra2 = 0;
-// VolumeTotal = 21.195
-
-// Medir em um copo graduado a quantidade de 100g (por ex) de areia
-// verificar quantos mL deu = 50ml
-// Converter para m3 = 0.00005 m3
-// dividir kg/m3 (calculo da densidade), no caso 0.1kg/X m3 (do copo graduado convertido em m3)0,000005
-// Sabendo o volume em m3 (leitura do sensor)
-// multiplicar m3 pela desidade (kg/m3) VolumeOcupado
-// m3 (sensor) x    Kg
-//                ------    = Kg
-//                  m3
-
 
 void setup() {
 
@@ -56,7 +44,7 @@ void DisplayProcedure()
   lcd.setCursor(0, 0);
   lcd.print("Distancia: ");
   lcd.print(mediaRound);
-  lcd.print("g");
+  lcd.print("cm");
   delay(10);
   lcd.setCursor(0, 1);
   lcd.print("Volume: ");
